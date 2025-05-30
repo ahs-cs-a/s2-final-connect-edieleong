580e050c8104ce1a9a83f07d3818a096d48a927f
AHS CS <ahs-cs@ausdk12.org>
2025-05-30 10:27:52 -0700

    crying fr

---
+++ SmartComputerPlayer.java
@@ -0,0 +1,9 @@
+public class SmartComputerPlayer extends ComputerPlayer {
+    public SmartComputerPlayer(String name, Color color) {
+        super(name, color);
+    }
+
+    public Move getMove(Board board) {
+        
+    }
+}
